ce9ebfdd83f9f728397f026540760fe34b80a266
AlbertAlvi <albertalvin99@gmail.com>
2024-07-04 12:12:40 -0300

    quarkus-jackson

--- 01-first-test/src/main/java/org/albert/endpoint/ProductEndpoint.java
+++ 01-first-test/src/main/java/org/albert/endpoint/ProductEndpoint.java
@@ -3,6 +3,7 @@ package org.albert.endpoint;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.albert.entity.Product;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,49 +11,59 @@ import java.util.List;
 @Path("/product")
 public class ProductEndpoint
 {
-    private static List<String> productList = new ArrayList<>(List.of("TV", "PC", "GPU"));
+    private static List<Product> productList = new ArrayList<>(List.of(
+            new Product(1L, "TV"),
+            new Product(2L, "GPU"),
+            new Product(3L, "CPU")
+    ));
 
     @GET
     @Path("/all")
-    @Produces(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
     public Response getAll()
     {
         return Response.ok(productList).build();
     }
 
     @POST
-    @Produces(MediaType.TEXT_PLAIN)
-    @Consumes(MediaType.TEXT_PLAIN)
-    // Because the product name is being passed in the body,
-    // you don't need to annotate String product
-    public Response save(String product)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    // Because the product is being passed in the body,
+    // you don't need to annotate the parameter of this method.
+    public Response save(Product product)
     {
         productList.add(product);
         return Response.ok(productList).build();
     }
 
     @DELETE
-    @Path("/{productName}")
-    public Response delete(@PathParam("productName") String productName)
+    @Path("/{id}")
+    public Response delete(@PathParam("id") Long id)
     {
-        return productList.remove(productName) ?
+        return productList.remove(new Product(id, null)) ?
                 Response.noContent().build() :
                 Response.status(Response.Status.BAD_REQUEST).build();
     }
 
     @PUT
-    @Path("/{oldName}")
-    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
     public Response update(
-            @PathParam("oldName") String oldName,
+            @PathParam("id") Long id,
             @QueryParam("newName") String newName
     )
     {
-        boolean rm = productList.remove(oldName);
-        // Always true according to documentation, so it's not necessary to add it to the ternary.
-        boolean add = productList.add(newName);
+        Product oldProduct = new Product(id, null);
+        int index = productList.indexOf(oldProduct);
 
-        return rm ?
+        boolean updated = false;
+        if (index != -1)
+        {
+            productList.set(index, new Product(id, newName));
+            updated = true;
+        }
+
+        return updated ?
                 Response.ok(productList).build() :
                 Response.status(Response.Status.NOT_FOUND).build();
     }
---
+++ 01-first-test/src/main/java/org/albert/entity/Product.java
@@ -0,0 +1,54 @@
+package org.albert.entity;
+
+import java.util.Objects;
+
+public class Product
+{
+    private Long id;
+    private String name;
+
+    public Product(Long id, String name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Product()
+    {
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(id);
+    }
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+}
