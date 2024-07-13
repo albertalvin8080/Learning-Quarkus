faa7c7501ecb62e404e8fdd3327a948501f5d719
AlbertAlvi <albertalvin99@gmail.com>
2024-07-13 11:04:55 -0300

    metrics

--- 09-health-metrics/src/main/java/org/albert/resource/UsersResource.java
+++ 09-health-metrics/src/main/java/org/albert/resource/UsersResource.java
@@ -8,7 +8,14 @@ import org.albert.proxy.UsersProxy;
 import org.eclipse.microprofile.config.ConfigProvider;
 import org.eclipse.microprofile.config.inject.ConfigProperties;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.metrics.MetricUnits;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Gauge;
+import org.eclipse.microprofile.metrics.annotation.Metered;
+import org.eclipse.microprofile.metrics.annotation.Timed;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import java.util.concurrent.TimeUnit;
 
 @Path("/users")
 public class UsersResource
@@ -27,7 +34,17 @@ public class UsersResource
     @RestClient
     UsersProxy usersProxy;
 
+    // http://localhost:8080/q/metrics/application
     @GET
+    @Counted(name = "countedGetAll",
+            description = "The number of times the getAll method is called.")
+    @Timed(name = "timedGetAll",
+        description = "How long it takes for getAll method to be executed.")
+    @Metered(name = "meteredGetAll",
+        description = "Tracks the frequency of getAll method invocations.")
+    @Gauge(name = "gaugeGetAll",
+            description = "Tracks a specific value for the getAll method.",
+            unit = MetricUnits.MILLISECONDS)
     @Produces(MediaType.APPLICATION_JSON)
     public Response getAll()
     {
